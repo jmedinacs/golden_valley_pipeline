88eae0a2ce96da0d0074dd9fc8b808c2f7669e27
John Medina <jmedinacs715@gmail.com>
2025-08-12 11:01:10 -0700

    Started on inspect_raw.py incorporating early check for null values and at the cups of standardizing text columns.

--- src/preprocess/inspect_raw.py
+++ src/preprocess/inspect_raw.py
@@ -4,5 +4,33 @@ Created on Aug 9, 2025
 @author: jarpy
 '''
 
+import util.main_data_io as mainIO
+
+
+def inspect_data(df):
+    """ """
+    print(df.info())
+    print("\nNumber of null entries per column\n")
+    print(df.isnull().sum())
+    
+def inspect_text_values(df, text_cols):
+    """ """
+    for col in text_cols:
+        print(df[col].value_counts(dropna=False))
+    
+
+
+def initial_inspection(client_name="missing_client",filename="missing_filename"):
+    """ """
+    # Load the raw data from the client's data/raw folder
+    df = mainIO.load_raw_data(client_name, filename)
+    # Initial inspection of data for data type and number of null
+    inspect_data(df)
+    
+    text_columns = ["employment_status","exempt_status"]
+    inspect_text_values(df, text_columns)
+    
+
+
 if __name__ == '__main__':
-    pass
+    initial_inspection(client_name="test_client",filename="jan_2024_test_data")
--- src/util/main_data_io.py
+++ src/util/main_data_io.py
@@ -6,3 +6,33 @@ Created on Aug 9, 2025
 
 import pandas as pd 
 import os
+
+def load_raw_data(client_name="missing_client",filename="missing_filename"):
+    """
+    Loads a raw CSV file from the '../../data/raw/' directory.
+
+    Parameters:
+        filename (str): Name of the raw CSV file (no extension)
+
+    Returns:
+        pd.DataFrame: Raw dataset
+
+    Raises:
+        FileNotFoundError: If the directory is missing
+    """
+    base_path = "../../data"
+    
+    if not os.path.exists(base_path):
+        raise FileNotFoundError(f"{filename}.csv client base path not found!")
+    
+    client_path = os.path.join(base_path, client_name)
+    if not os.path.exists(client_path):
+        raise FileNotFoundError(f"{client_path} not found!")
+    
+    file_path = os.path.join(client_path,f"data/raw/{filename}.csv")
+    df = pd.read_csv(file_path)
+    
+    print(f"{filename}.csv loaded")
+    return df
+    
+    
